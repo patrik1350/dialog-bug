1134adf0fccc24cb8665134dbd6be1b45113a992
Patrik Golity <patrik.golity@minero.hu>
2023-05-25 10:27:19 +0200

    dialog bug

--- src/main/java/com/example/application/views/main/MainView.java
+++ src/main/java/com/example/application/views/main/MainView.java
@@ -1,32 +1,25 @@
 package com.example.application.views.main;
 
-import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.dialog.Dialog;
+import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
-import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.PreserveOnRefresh;
 import com.vaadin.flow.router.Route;
 
 @PageTitle("Main")
 @Route(value = "")
+@PreserveOnRefresh
 public class MainView extends HorizontalLayout {
 
-    private TextField name;
-    private Button sayHello;
-
     public MainView() {
-        name = new TextField("Your name");
-        sayHello = new Button("Say hello");
-        sayHello.addClickListener(e -> {
-            Notification.show("Hello " + name.getValue());
+        Button button = new Button("Open dialog");
+        button.addClickListener(e -> {
+            Dialog dialog = new Dialog(new Label("Dialog"));
+            dialog.open();
+            dialog.addDialogCloseActionListener(event -> System.out.println("Closing dialog"));
         });
-        sayHello.addClickShortcut(Key.ENTER);
-
-        setMargin(true);
-        setVerticalComponentAlignment(Alignment.END, name, sayHello);
-
-        add(name, sayHello);
+        add(button);
     }
-
 }
